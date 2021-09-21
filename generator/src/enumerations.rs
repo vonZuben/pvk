cd420b9488a1e54f55db55b08ccc7a753aa4124e
Chris von Zuben <chris.vonzuben@gmail.com>
2021-09-22 01:27:14 +0900

    BIG DELETE
    get rid of most of the old code
    make way for the new

--- generator/src/enumerations.rs
+++ generator/src/enumerations.rs
@@ -5,7 +5,6 @@ use proc_macro2::{TokenStream};
 
 use crate::utils::*;
 use crate::utils;
-use crate::global_data;
 
 use crate::constants;
 
@@ -53,7 +52,7 @@ impl ToTokens for EnumVariants<'_> {
 }
 
 pub fn make_variant_name(enumeration_name: &str, varient_name: &str) -> String {
-    let extension_tags = global_data::extension_tags();
+    let extension_tags = std::collections::HashMap::<&str,&str>::new();
 
     // all tags are upper case
     // find the "last" lower case letter and then assume that anything following might be a tag
@@ -94,88 +93,6 @@ pub fn make_variant_name(enumeration_name: &str, varient_name: &str) -> String {
     }
 }
 
-pub fn handle_enumerations<'a>(enumerations: &'a Enums) -> TokenStream {
-
-    let q = enumerations.elements.iter().filter_map( |elem| match elem {
-        EnumsElement::Enumeration(enm) => {
-
-            let name = enm.name.as_code();
-
-            // if the type is flagbits, then it was already delared
-            // during definitions as bitmask
-            let type_decleration =
-                if enm.name.contains("FlagBits") {
-                    quote!()
-                }
-                else {
-                    quote!{
-                        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-                        #[repr(transparent)]
-                        pub struct #name(pub(crate) i32);
-                    }
-                };
-
-            let vals = enm.elements.iter().filter_map( |elem| {
-                match elem {
-                    EnumerationElement::Enum(enum_constant) => {
-
-                        let const_name = make_variant_name(&enm.name, enum_constant.name.as_str()).as_code();
-
-                        let val = one_option!(
-
-                            &enum_constant.number , |num: &i32| num.to_string().as_code() ;
-                            &enum_constant.hex , |hex_str| format!("0x{:0>8}", hex_str).as_code() ;
-                            &enum_constant.bitpos , |bitpos: &u32| format!("0x{:0>8X}", (1u32 << bitpos)).as_code() ;
-                            &enum_constant.c_expression , |_| panic!("error: c_expression for enumeration val") ;
-
-                            );
-
-                        Some( quote!{ pub const #const_name: Self = #name(#val); } )
-                    },
-                    EnumerationElement::Notation(_notation) => None,
-                    EnumerationElement::UnusedRange(_range) => None,
-                }
-            });
-
-            // TODO add code to safely create from raw
-            //
-            //let possible_varients = enm.elements.iter().filter_map( |elem| {
-            //    match elem {
-            //        EnumerationElement::Enum(enum_constant) => {
-            //            let const_name = make_varient_name(&enm, &enum_constant);
-            //            Some( quote!( Self::#const_name => true, ) )
-            //        },
-            //        EnumerationElement::Notation(_notation) => None,
-            //        EnumerationElement::UnusedRange(_range) => None,
-            //    }
-            //});
-
-            //let to_from_raw_code = if enm.name.contains("FlagBits") {
-            //    impl TryFrom<Flags> for #name {
-            //        type Error = &'static str;
-            //        fn try_from(val: Flags) -> Result<Self, Self::Error> {
-            //            if let Some(valid) = match Self(val)
-            //        }
-            //    }
-            //}
-            //else {
-            //};
-
-            let q = quote!{
-                #type_decleration
-                impl #name {
-                    #( #vals )*
-                }
-            };
-
-            Some(q)
-        },
-        EnumsElement::Notation(_) => None,
-    });
-
-    quote!( #( #q )* )
-
-}
 
 pub fn make_enumeration_display_code<'a>(enums: &'a [(&String, &Vec<String>)]) -> impl Iterator<Item=TokenStream> + 'a {
 
--- generator/src/features.rs
+++ generator/src/features.rs
@@ -9,7 +9,6 @@ use proc_macro2::{TokenStream};
 
 use crate::utils::*;
 use crate::commands::*;
-use crate::global_data;
 
 // =================================================================
 #[derive(Copy, Clone)]
@@ -173,92 +172,5 @@ fn parse_version(ver: &str) -> TokenStream {
     // Note: I am assuming that the major and minor that are parsed are integers
 
     quote!( vk_make_version(#major, #minor, 0) )
-
-}
-
-pub fn handle_features(features: &Features, parse_state: &mut crate::ParseState) -> TokenStream {
-
-    // a given feature should also load all previous features
-    //
-    // e.g. 1.1 only specifies things added in 1.1 and still needs
-    // to load things specified in 1.0
-    //
-    // by including the previous feature in every feature
-    // we recursively load everything
-    //
-    // using parse_state to keep track of this
-    //
-    // NOTE this assumes that all features will be parsed in order of earliest to latest
-
-    let q = features.elements.iter().map(|feature| {
-
-        let name = feature.name.as_code();
-
-        let ver = parse_version(&feature.name);
-
-        let requiered_command_names = feature.elements.iter().filter_map(
-            |feature_elem| match feature_elem {
-                FeatureElement::Require(spec) => Some(spec.elements.iter()),
-                _ => None,
-            }).flatten()
-        .filter_map(|feature_ref| match feature_ref {
-            FeatureReference::CommandReference(cmd_ref) => Some(&cmd_ref.name),
-            _ => None,
-        });
-
-        macro_rules! filter_global_command_type{
-            ( $varient:path ) => {
-                |val| match global_data::command_type(val) {
-                    $varient => true,
-                    _ => false,
-                }
-            }
-        }
-        let instance_commands = requiered_command_names.clone()
-            .filter(filter_global_command_type!(CommandCategory::Instance)).map(StrAsCode::as_code);
-        let device_commands = requiered_command_names.clone()
-            .filter(filter_global_command_type!(CommandCategory::Device)).map(StrAsCode::as_code);
-
-        let previous_feature_instance = &parse_state.previous_feature_instance;
-        let previous_feature_device = &parse_state.previous_feature_device;
-
-        let q = quote!{
-
-            #[derive(Clone, Copy, Debug)]
-            pub struct #name;
-
-            impl FeatureCore for #name {
-                fn load_instance_commands(&self, instance: Instance, inst_cmds: &mut InstanceCommands) {
-                    let loader = |raw_cmd_name: &CStr| unsafe { GetInstanceProcAddr(instance, raw_cmd_name.to_c()) };
-                    #( inst_cmds.#instance_commands.load(loader); )*
-                    #previous_feature_instance
-                }
-                fn load_device_commands(&self, device: Device, dev_cmds: &mut DeviceCommands) {
-                    let loader = |raw_cmd_name: &CStr| unsafe { GetDeviceProcAddr(device, raw_cmd_name.to_c()) };
-                    #( dev_cmds.#device_commands.load(loader); )*
-                    #previous_feature_device
-                }
-                fn version(&self) -> u32 {
-                    #ver
-                }
-                fn clone_feature(&self) -> Box<dyn Feature> {
-                    Box::new(self.clone())
-                }
-            }
-        };
-
-        parse_state.previous_feature_instance = Some(quote!{
-            let previous_feature = #name;
-            previous_feature.load_instance_commands(instance, inst_cmds);
-        });
-        parse_state.previous_feature_device = Some(quote!{
-            let previous_feature = #name;
-            previous_feature.load_device_commands(device, dev_cmds);
-        });
-        q
-
-    });
-
-    quote!( #( #q )* )
 
 }
