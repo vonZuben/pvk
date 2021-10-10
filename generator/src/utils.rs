c1a4248b2e62685f944c9054d2e3369526183562
Chris von Zuben <chris.vonzuben@gmail.com>
2021-10-10 23:15:05 +0900

    Fix VecMap iter method
    previous commit was missing part

--- generator/src/utils.rs
+++ generator/src/utils.rs
@@ -64,6 +64,7 @@ impl<K: Eq + Hash, V> VecMap<K, V> {
     pub fn contains_or_default(&mut self, key: K, default: V) {
         let _ = self.get_mut_or_default(key, default);
     }
+    pub fn iter<'a>(&'a self) -> impl Iterator<Item=&'a V> + Clone {
         self.vec.iter()
     }
 }
