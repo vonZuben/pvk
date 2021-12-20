13b0b7f387a345d8cd19c52130510cb369a8633a
Chris von Zuben <chris.vonzuben@gmail.com>
2021-12-21 03:46:19 +0900

    fix make_extention_implementor

--- vk-safe-sys/src/extension.rs
+++ vk-safe-sys/src/extension.rs
@@ -71,14 +71,11 @@ pub mod device {
 }
 
 macro_rules! make_extention_implementor {
-    ( $m_name:ident => $($iex:ident),* ; $($dex:ident),* ) => {
+    ( $m_name:ident => $($command:ident),* ) => {
         #[macro_export]
         macro_rules! $m_name {
-            ( @INSTANCE_COMMANDS $name:ident ) => {
-                $crate::impl_fptr_traits!($name => $($iex),*);
-            };
-            ( @DEVICE_COMMANDS $name:ident ) => {
-                $crate::impl_fptr_traits!($name => $($dex),*);
+            ( $name:ident ) => {
+                $crate::impl_fptr_traits!($name => $($command),*);
             };
         }
     };
@@ -86,12 +83,12 @@ macro_rules! make_extention_implementor {
 
 macro_rules! use_instance_and_device_extension_commands {
     ( @EXTENSIONS $($extension:ident),* ) => {
-        // $(
-        //     $extension!( @ALL_COMMANDS use_instance_and_device_extension_commands @INNER $extension => );
-        // )*
+        $(
+            $extension!( @ALL_COMMANDS use_instance_and_device_extension_commands @INNER $extension => );
+        )*
     };
-    ( @INNER $extension:ident => $($iex:ident),* ; $($dex:ident),* ) => {
-        make_extention_implementor!( $extension => $($iex),* ; $($dex),* );
+    ( @INNER $extension:ident => $($command:ident),* ) => {
+        make_extention_implementor!( $extension => $($command),* );
     };
     () => {
         use_all_vulkan_extension_names!(use_instance_and_device_extension_commands @EXTENSIONS);
