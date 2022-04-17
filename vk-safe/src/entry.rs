23c75d4ffbd657d8f629c59986bcb6d732426dac
Chris von Zuben <chris.vonzuben@gmail.com>
2022-04-18 00:14:38 +0900

    Clean up warnings

--- vk-safe/src/entry.rs
+++ vk-safe/src/entry.rs
@@ -1,4 +1,3 @@
-use std::marker::PhantomData;
 use std::ffi::CStr;
 use std::mem::MaybeUninit;
 use std::convert::TryInto;
@@ -18,8 +17,6 @@ use vk::{
     commands, 
     version::Version
 };
-
-use crate::utils::{VkVersion};
 
 /// This is the very first point of entry that is internally used to load all other functions
 #[link(name = "vulkan")]
@@ -124,82 +121,3 @@ EnumerateInstanceLayerProperties {
         ()
     );
 }}
-
-//======================================
-
-
-
-
-
-/// Vulkan Instance handle
-/// this struct holds all the instance level commands for both the loaded Vulkan Version (Feature)
-/// and the loaded extensions
-pub struct Instance<Version, Extensions> {
-    /// raw handle
-    handle: vk::Instance,
-    /// instance commands for Version
-    version: Version,
-    /// extension commands for loaded extensions
-    extensions: Extensions,
-}
-
-/// Builder for creating an Instance
-pub struct InstanceCreator<'a, Version, Extensions> {
-    version: PhantomData<Version>,
-    extensions: PhantomData<Extensions>,
-    app_name: Option<&'a CStr>,
-    app_version: VkVersion,
-    engine_name: Option<&'a CStr>,
-    engine_version: VkVersion,
-}
-
-// impl<Version, Extensions> InstanceCreator<'_, Version, Extensions>
-// where
-//     Version: vk::LoadCommands + vk::version::Version,
-//     Extensions: vk::LoadCommands,
-// {
-//     pub fn create(&self) -> Instance<Version, Extensions> {
-//         let api_version: VkVersion = Version::VersionTuple.into();
-
-//         let app_info = vk::ApplicationInfo {
-//             s_type: vk::StructureType::APPLICATION_INFO,
-//             p_next: std::ptr::null(),
-//             api_version: api_version.raw(),
-//             p_application_name: self.app_name.as_c_ptr(),
-//             application_version: self.app_version.raw(),
-//             p_engine_name: self.engine_name.as_c_ptr(),
-//             engine_version: self.engine_version.raw(),
-//         };
-
-//         todo!();
-//     }
-// }
-
-// impl<Version, Extensions> Default for InstanceCreator<'_, Version, Extensions>
-// where
-//     Version: vk::LoadCommands,
-//     Extensions: vk::LoadCommands,
-// {
-//     fn default() -> Self {
-//         Self {
-//             version: Default::default(),
-//             extensions: Default::default(),
-//             app_name: Default::default(),
-//             app_version: Default::default(),
-//             engine_name: Default::default(),
-//             engine_version: Default::default(),
-//         }
-//     }
-// }
-
-pub fn instance_creator<'a, Version: vk::LoadCommands + vk::version::Version, Extensions: vk::LoadCommands>(
-) -> InstanceCreator<'a, Version, Extensions> {
-    InstanceCreator {
-        version: Default::default(),
-        extensions: Default::default(),
-        app_name: Default::default(),
-        app_version: Default::default(),
-        engine_name: Default::default(),
-        engine_version: Default::default(),
-    }
-}
--- vk-safe/src/utils.rs
+++ vk-safe/src/utils.rs
@@ -1,12 +1,7 @@
 use std::fmt;
-use std::marker::PhantomData;
-use std::os::raw::c_char;
-use std::ffi::CStr;
-use std::ptr::NonNull;
-
 // // =================Custom Result=======================
 // // there is more than one success value in Vulkan, which is important to know for some APIs
-// // so we need to keep it along with any actual returend values
+// // so we need to keep it along with any actual returned values
 // pub enum VkResult<T> {
 //     Ok(T, vk_safe_sys::Result),
 //     Err(vk_safe_sys::Result),
@@ -82,6 +77,7 @@ impl VkVersion {
     pub fn parts(&self) -> (u32, u32, u32, u32) {
         (self.0 >> 29, (self.0 >> 22) & 0x7F, (self.0 >> 12) & 0x3FF, self.0 & 0xFFF)
     }
+    #[allow(unused)]
     pub fn raw(&self) -> u32 {
         self.0
     }
@@ -97,75 +93,5 @@ impl fmt::Debug for VkVersion {
 impl From<(u32, u32, u32)> for VkVersion {
     fn from((major, minor, patch): (u32, u32, u32)) -> Self {
         Self::new(0, major, minor, patch)
-    }
-}
-
-// =================VkStr===========================
-
-/// This must be compatible with char* in C so that it can be used in arrays of c strings
-/// Also ensure that representation with Option can make use of null niche so that
-/// Option<VkStr> can be cheaply converted and even maybe used directly in the ffi interface
-/// 
-/// Also important, the string must never be mutated from this
-// #[repr(transparent)]
-// #[derive(Clone, Copy)]
-// pub struct VkStr<'a> {
-//     ptr: NonNull<c_char>,
-//     _p: PhantomData<&'a c_char>,
-// }
-
-// impl<'a> VkStr<'a> {
-//     pub fn ptr(&self) -> *const c_char {
-//         self.ptr.as_ptr()
-//     }
-// }
-
-// impl<'a> From<&'a CStr> for VkStr<'a> {
-//     fn from(from: &'a CStr) -> Self {
-//         // CStr: "This type represents a borrowed reference to a nul-terminated array of bytes"
-//         // thus, we should beable to make NonNull
-//         // VkStr should not ever mutate the string, so taking the *mut should be fine
-//         Self {
-//             ptr: unsafe { NonNull::new_unchecked(from.as_ptr() as *mut c_char) },
-//             _p: PhantomData,
-//         }
-//     }
-// }
-
-// =================Buffer type that people can use to pass their own allocated space===============
-// for som APIs, want to provide the option for the user to provide their own allocation to be filled in
-// However, this is kind of difficult to design, because I don't even know all possible reasons to want this (should I even be doing this????)
-//
-// Just keep this as a work in progress for now
-// we can use it later if usful
-pub trait BufferInner<T> : AsRef<[T]> + AsMut<[T]> {}
-impl<T, B> BufferInner<T> for B where B: AsRef<[T]> + AsMut<[T]> {}
-
-pub struct Buffer<B, T> {
-    buffer: B,
-    len: usize,
-    _p: PhantomData<[T]>,
-}
-
-impl<T, B: BufferInner<T>> Buffer<B, T> {
-    pub fn uninit(buffer: B) -> Self {
-        Self {
-            buffer,
-            len: 0,
-            _p: PhantomData,
-        }
-    }
-}
-
-impl<T, B: BufferInner<T>> From<B> for Buffer<B, T> {
-    fn from(buffer: B) -> Self {
-        Self::uninit(buffer)
-    }
-}
-
-impl<T, B: BufferInner<T>> std::ops::Deref for Buffer<B, T> {
-    type Target = [T];
-    fn deref(&self) -> &Self::Target {
-        &self.buffer.as_ref()[..self.len]
     }
 }
