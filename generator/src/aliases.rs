0778689bbb1c79998f0cfb1c6bf1793cbb17a7a1
Chris von Zuben <chris.vonzuben@gmail.com>
2022-07-20 21:39:57 +0900

    more krs_quote

--- generator/src/aliases.rs
+++ generator/src/aliases.rs
@@ -1,6 +1,9 @@
 
 use quote::{ToTokens, quote};
 
+use krs_quote::{my_quote, my_quote_with};
+use crate::utils::ToTokensInterop;
+
 use crate::{definitions::TypeDef, utils::{StrAsCode, VecMap}};
 
 pub struct CmdAliasNames<I> {
@@ -20,12 +23,27 @@ impl<'a, I: Iterator<Item=TypeDef> + Clone> ToTokens for CmdAliasNames<I> {
         let name = self.alias_defs.clone().map(|td|td.name);
         let alias = self.alias_defs.clone().map(|td|td.ty);
 
-        quote!(
+        my_quote!(
             macro_rules! use_cmd_alias_pairs {
                 ( $call:ident $($pass:tt)* ) => {
-                    $call!( $($pass)* #( #name = #alias ),* );
+                    $call!( $($pass)* {@,* {@name} = {@alias} } );
                 }
             }
-        ).to_tokens(tokens);
+        ).to_tokens_interop(tokens);
+    }
+}
+
+impl<'a, I: Iterator<Item=TypeDef> + Clone> krs_quote::ToTokens for CmdAliasNames<I> {
+    fn to_tokens(&self, tokens: &mut krs_quote::TokenStream) {
+        let name = self.alias_defs.clone().map(|td|td.name);
+        let alias = self.alias_defs.clone().map(|td|td.ty);
+
+        my_quote_with!( tokens {
+            macro_rules! use_cmd_alias_pairs {
+                ( $call:ident $($pass:tt)* ) => {
+                    $call!( $($pass)* {@,* {@name} = {@alias} } );
+                }
+            }
+        });
     }
 }
