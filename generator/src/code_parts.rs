dc555d86d536bd380d912bd4e8cb7e85b0597cc1
Chris von Zuben <chris.vonzuben@gmail.com>
2023-02-07 18:56:00 +0900

    add doc for code_parts

--- generator/src/code_parts.rs
+++ generator/src/code_parts.rs
@@ -1,4 +1,8 @@
-
+/// Provide a list of generated code parts for other macros as a list of coma separated identifiers
+///
+/// $run : macro which can parse a list of coma separated identifiers at the end of the input tokens
+/// ($semi?) : if $run will output an 'item', then put a semi colon here for proper syntax
+/// $pass* : the rest of the input tokens are passed to the $run macro before the list of module names
 macro_rules! code_parts {
     ( $run:ident ($($semi:tt)?) $($pass:tt)* ) => {
         $run!( $($pass)*
