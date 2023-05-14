60d47f60acd10fe3af6c007da0c641ecd92dd739
Chris von Zuben <chris.vonzuben@gmail.com>
2023-05-15 01:20:19 +0900

    forgot to delete some old docs

--- vk-safe/src/entry/enumerate_instance_layer_properties.rs
+++ vk-safe/src/entry/enumerate_instance_layer_properties.rs
@@ -23,17 +23,7 @@ impl fmt::Debug for LayerProperties {
 }
 
 /*
-SAFETY (https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkEnumerateInstanceLayerProperties.html)
-
-VUID-vkEnumerateInstanceLayerProperties-pPropertyCount-parameter
-pPropertyCount must be a valid pointer to a uint32_t value
-
-- internally handled with a &mut u32
-
-VUID-vkEnumerateInstanceLayerProperties-pProperties-parameter
-If the value referenced by pPropertyCount is not 0, and pProperties is not NULL, pProperties must be a valid pointer to an array of pPropertyCount VkLayerProperties structures
-
-- internally handled using EnumeratorStorage, rust slices, and len of such slices
+https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkEnumerateInstanceLayerProperties.html
 */
 impl_safe_entry_interface! {
 EnumerateInstanceLayerProperties {
