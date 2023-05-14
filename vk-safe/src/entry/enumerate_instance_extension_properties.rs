e90d3212097d5db2acd487e5ee5a8abd15fc5403
Chris von Zuben <chris.vonzuben@gmail.com>
2023-05-15 01:15:49 +0900

    use vuids for EnumerateInstanceExtensionProperties

--- vk-safe/src/entry/enumerate_instance_extension_properties.rs
+++ vk-safe/src/entry/enumerate_instance_extension_properties.rs
@@ -1,7 +1,8 @@
 use super::command_impl_prelude::*;
 
-use std::ffi::CStr;
 use std::fmt;
+
+use crate::vk_str::VkStr;
 
 //===========ExtensionProperties
 simple_struct_wrapper!(ExtensionProperties);
@@ -20,24 +21,40 @@ impl fmt::Debug for ExtensionProperties {
 }
 
 /*
-SAFETY (https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkEnumerateInstanceExtensionProperties.html)
-
-VUID-vkEnumerateInstanceExtensionProperties-pLayerName-parameter
-If pLayerName is not NULL, pLayerName must be a null-terminated UTF-8 string
-
-- Option<&CStr> TODO, does CStr ensure null-terminated UTF-8 string?
-
-VUID-vkEnumerateInstanceExtensionProperties-pPropertyCount-parameter
-pPropertyCount must be a valid pointer to a uint32_t value
-
-- internally handled with a &mut u32
-
-VUID-vkEnumerateInstanceExtensionProperties-pProperties-parameter
-If the value referenced by pPropertyCount is not 0, and pProperties is not NULL, pProperties must be a valid pointer to an array of pPropertyCount VkExtensionProperties structures
-
-- internally handled using EnumeratorStorage, rust slices, and len of such slices
+https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkEnumerateInstanceExtensionProperties.html
 */
 impl_safe_entry_interface! {
 EnumerateInstanceExtensionProperties {
-    enumerator_code!(enumerate_instance_extension_properties(layer_name: Option<&CStr>) -> ExtensionProperties);
+    enumerator_code!(enumerate_instance_extension_properties(layer_name: Option<VkStr<'_>>) -> ExtensionProperties);
 }}
+
+mod enumerate_instance_extension_properties_validation {
+    use vk_safe_sys::validation::EnumerateInstanceExtensionProperties::*;
+
+    pub struct Validation;
+
+    #[allow(non_upper_case_globals)]
+    impl Vuids for Validation {
+        const VUID_vkEnumerateInstanceExtensionProperties_pLayerName_parameter: () = {
+            // verified by Option<VkStr>
+        };
+
+        const VUID_vkEnumerateInstanceExtensionProperties_pPropertyCount_parameter: () ={
+            // handled by enumerator_code!()
+        };
+
+        const VUID_vkEnumerateInstanceExtensionProperties_pProperties_parameter: () = {
+            // handled by enumerator_code!()
+        };
+    }
+
+    check_vuid_defs!(
+        pub const VUID_vkEnumerateInstanceExtensionProperties_pLayerName_parameter:
+            &'static [u8] =
+            "If pLayerName is not NULL, pLayerName must be a null-terminated UTF-8 string"
+                .as_bytes();
+        pub const VUID_vkEnumerateInstanceExtensionProperties_pPropertyCount_parameter:
+            &'static [u8] = "pPropertyCount must be a valid pointer to a uint32_t value".as_bytes();
+        pub const VUID_vkEnumerateInstanceExtensionProperties_pProperties_parameter : & 'static [ u8 ] = "If the value referenced by pPropertyCount is not 0, and pProperties is not NULL, pProperties must be a valid pointer to an array of pPropertyCount VkExtensionProperties structures" . as_bytes ( ) ;
+    );
+}
