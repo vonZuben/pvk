0097909d5d2de3a88426446b45473ccdc01d1645
Chris von Zuben <chris.vonzuben@gmail.com>
2023-05-15 18:31:52 +0900

    use vuids for GetPhysicalDeviceFeatures

--- vk-safe/src/physical_device/get_physical_device_features.rs
+++ vk-safe/src/physical_device/get_physical_device_features.rs
@@ -6,18 +6,10 @@ use crate::instance::InstanceConfig;
 use std::mem::MaybeUninit;
 use std::fmt;
 
+use vk_safe_sys::validation::GetPhysicalDeviceFeatures::*;
+
 /*
 https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkGetPhysicalDeviceFeatures.html
-
-VUID-vkGetPhysicalDeviceFeatures-physicalDevice-parameter
-physicalDevice must be a valid VkPhysicalDevice handle
-
-- provided by vk_safe::PhysicalDevice
-
-VUID-vkGetPhysicalDeviceFeatures-pFeatures-parameter
-pFeatures must be a valid pointer to a VkPhysicalDeviceFeatures structure
-
-- provided with MaybeUninit
 */
 impl<C: InstanceConfig> PhysicalDevice<'_, C> where C::InstanceCommands: vk::GetCommand<vk::GetPhysicalDeviceFeatures> {
     pub fn get_physical_device_features(&self) -> PhysicalDeviceFeatures {
@@ -36,3 +28,23 @@ impl fmt::Debug for PhysicalDeviceFeatures {
         self.inner.fmt(f)
     }
 }
+
+struct Validation;
+
+#[allow(non_upper_case_globals)]
+impl Vuids for Validation {
+    const VUID_vkGetPhysicalDeviceFeatures_physicalDevice_parameter: () = {
+        // ensured by PhysicalDevice creation
+    };
+
+    const VUID_vkGetPhysicalDeviceFeatures_pFeatures_parameter: () = {
+        // using MaybeUninit
+    };
+}
+
+check_vuid_defs!(
+    pub const VUID_vkGetPhysicalDeviceFeatures_physicalDevice_parameter: &'static [u8] =
+    "physicalDevice must be a valid VkPhysicalDevice handle".as_bytes();
+    pub const VUID_vkGetPhysicalDeviceFeatures_pFeatures_parameter: &'static [u8] =
+    "pFeatures must be a valid pointer to a VkPhysicalDeviceFeatures structure".as_bytes();
+);
