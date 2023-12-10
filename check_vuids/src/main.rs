0022efdc5552f9abae7c6ca1346156f5dd1fb775
Chris von Zuben <chris.vonzuben@gmail.com>
2023-12-10 15:35:33 +0900

    only use default path if proper arg not provided

--- check_vuids/src/main.rs
+++ check_vuids/src/main.rs
@@ -1,4 +1,4 @@
-use std::env::args_os;
+use std::env::{args_os, current_exe};
 use std::path::PathBuf;
 
 mod file_edits;
@@ -11,17 +11,32 @@ type Error = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Error>;
 
 const USAGE_ERROR: &'static str =
-    "USAGE: run from workspace root or provide path to directory to check";
+    "USAGE: run from workspace root or provide path to directory to check (note: expects exactly zero on one arguments)";
+
+fn check_in_workspace() -> Option<PathBuf> {
+    let check_dir = PathBuf::from("vk-safe/src");
+
+    if check_dir.exists() {
+        Some(check_dir)
+    } else {
+        None
+    }
+}
 
 /**
 Check VUIDs in all files in a given directory
  */
 fn main() -> Result<()> {
-    let mut check_dir = PathBuf::from("vk-safe/src");
-
-    if !check_dir.exists() {
-        check_dir = args_os().last().ok_or(USAGE_ERROR)?.into();
-    }
+    let check_dir = match args_os().last() {
+        Some(arg) => {
+            if arg == current_exe()? {
+                check_in_workspace().ok_or(USAGE_ERROR)?
+            } else {
+                arg.into()
+            }
+        }
+        None => check_in_workspace().ok_or(USAGE_ERROR)?,
+    };
 
     if !check_dir.is_dir() {
         Err(USAGE_ERROR)?
