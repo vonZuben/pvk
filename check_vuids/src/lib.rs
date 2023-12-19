ea9fb540d85332a9b19beca8971f99a9eb67d3e6
Chris von Zuben <chris.vonzuben@gmail.com>
2023-12-20 00:40:19 +0900

    better check_vuids docs

--- check_vuids/src/lib.rs
+++ check_vuids/src/lib.rs
@@ -1,63 +1,61 @@
 #![warn(missing_docs)]
 
+//! Macros that are used by the check_vuids tool
+//!
+//! Add [check_vuids!] macro calls, e.g. `check_vuids!(CreateInstance)`, at the locations
+//! where you want to check the VUIDs for a particular Vulkan item. Then use `cargo run --bin check_vuids` and all files
+//! will be checked for check_vuids! and will ensure all VUID descriptions for the specified items are up to date by automatically adding/updating
+//! [version!], [cur_description!], and [old_description!] as appropriate.
+//!
+//! A VUID's name is represented as a block label, and the contents of the block is the VUID information.
+//!
+//! The macros only provide information to the reader. User must ensure that each VUID invariant is satisfied with any appropriate means.
+//!
 /*!
-This module includes macros that are used by the check_vuids tool
+# Example
+```
+pub fn create_instance() -> Instance {
+    check_vuids::check_vuids!(CreateInstance);
 
-the macros can be inserted into rust source code files to provide information about what VUIDs are being checked
-the check_vuids tool will parse the rust code files and check to see that the VUIDs are up to date
+    #[allow(unused_labels)]
+    'VUID_vkCreateInstance_ppEnabledExtensionNames_01388: {
+        check_vuids::version! {"1.3.268"}
+        check_vuids::cur_description! {
+        "All required extensions for each extension in the VkInstanceCreateInfo::ppEnabledExtensionNames"
+        "list must also be present in that list"
+        }
 
-the macros generate no actual code and only provide information to the reader and check_vuids tool
- */
+        // user added check
+    }
+}
+```
+*/
 
-/// indicates start of a list of VUIDs to check
+/// Start of a list of VUIDs to check
 ///
-/// must be placed within a block
-/// all VUIDs to check must follow the macro call within the same block
+/// Must be placed within a block. All VUIDs to check must follow this macro call within the same block
 #[macro_export]
 macro_rules! check_vuids {
     ($name:ident) => {};
 }
 
-/// indicate the version of the VUID to check
-/// the check_vuids tool will check if this is up to date with the latest version that check_vuids was compiled with
+/// Version of the VUID to check
+/// check_vuids will check if this is up to date with the latest version that check_vuids was compiled with
 #[macro_export]
 macro_rules! version {
     ($ver:literal) => {};
 }
 
-/// indicate the current description of the VUID to check
-/// the check_vuids tool will check if this is up to date with the latest version that check_vuids was compiled with
+/// Current description of the VUID to check
+/// check_vuids will check if this is up to date with the latest version that check_vuids was compiled with
 #[macro_export]
 macro_rules! cur_description {
     ($($desc:literal)*) => {};
 }
 
-/// provide the old description of the VUID
-/// when the check_vuids toll updates the description of a VUID, it will save the old description with this
-/// this is simply informative to the user to allow them to easily compare and see what is new
+/// Previous description of the VUID from before an update
+/// Allows easy comparison with the updated description to see what is new
 #[macro_export]
 macro_rules! old_description {
     ($($desc:literal)*) => {};
 }
-
-/* EXAMPLE
-const fn tst() {
-    #![allow(unused_labels)]
-    check_vuids!(CreateInstance);
-
-    'VUID_INFO_003245: {
-        version!("1.3.24");
-        cur_description!("Info must be valid");
-
-        compile_error!("new VUID")
-    }
-
-    'VUID_INFO_001111: {
-        version!("1.3.24");
-        cur_description!("Info must be valid and ...");
-        old_description!("Info must be valid");
-
-        compile_error!("updated VUID")
-    }
-}
- */
