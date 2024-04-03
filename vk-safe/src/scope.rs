2f592387c698d3ae59f2591f3463f2dd82d9617f
Chris von Zuben <chris.vonzuben@gmail.com>
2024-04-03 19:53:39 +0900

    update the internal representation for RefScope
    removes unnecessary indirection and raw pointer

--- vk-safe/src/scope.rs
+++ vk-safe/src/scope.rs
@@ -8,6 +8,7 @@ use std::marker::PhantomData;
 ///
 /// this can be used to ensure different things should have the same Id
 #[derive(Default, Clone, Copy)]
+#[repr(transparent)]
 pub struct ScopeId<'id>(PhantomData<*mut &'id ()>);
 
 /// Wrapper for a reference to a type that should be limited to a specific scope marked by Id
@@ -15,27 +16,27 @@ pub struct ScopeId<'id>(PhantomData<*mut &'id ()>);
 /// types which are only safe within a scope implement their methods through this wrapper
 /// (e.g. impl Scope<'id, Instance> {fn enumerate_physical_devices()})
 #[repr(transparent)]
-pub struct Scope<'scope, H> {
-    handle: &'scope H,
+pub struct Scope<'scope, T> {
+    handle: &'scope T,
     _id: ScopeId<'scope>,
 }
 
-impl<H> Clone for Scope<'_, H> {
+impl<T> Clone for Scope<'_, T> {
     fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<H> Copy for Scope<'_, H> {}
+impl<T> Copy for Scope<'_, T> {}
 
-impl<H: std::fmt::Debug> std::fmt::Debug for Scope<'_, H> {
+impl<T: std::fmt::Debug> std::fmt::Debug for Scope<'_, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.handle.fmt(f)
     }
 }
 
-impl<'id, H> Scope<'id, H> {
-    pub(crate) fn new_scope(handle: &'id H) -> Self {
+impl<'id, T> Scope<'id, T> {
+    pub(crate) fn new_scope(handle: &'id T) -> Self {
         Self {
             handle,
             _id: Default::default(),
@@ -43,15 +44,14 @@ impl<'id, H> Scope<'id, H> {
     }
 }
 
-impl<H> std::ops::Deref for Scope<'_, H> {
-    type Target = RefScope<Self, H>;
+impl<T> std::ops::Deref for Scope<'_, T> {
+    type Target = RefScope<Self, T>;
     fn deref(&self) -> &Self::Target {
         // this is safe because:
-        // - Scope and RefScope are repr(transparent)
-        // - Scope is a &'_ H
-        // - RefScope is *const H
-        // - &'_ H and *const H have same memory layout
-        unsafe { std::mem::transmute::<&Self, &RefScope<Self, H>>(self) }
+        // - RefScope repr(transparent) wrapper for H
+        // - Scope repr(transparent) wrapper for &'_ H
+        // - Scope == &RefScope == &'_ H
+        unsafe { std::mem::transmute::<Self, &RefScope<Self, T>>(*self) }
     }
 }
 
@@ -60,70 +60,40 @@ impl<H> std::ops::Deref for Scope<'_, H> {
 /// This type will be internally constructed to be RefScope<Scope<'scope, T>, T>
 /// this is a trick to make the lifetime parameter vanish, while still keeping the lifetime information
 ///
-/// I currently strongly believe that this should be completely sound. RefScope should essentially be equivalent to
-/// a &'scope T. However, there may be some issue regarding stacked borrows as noted below?
-///
-/// At this time, according to https://doc.rust-lang.org/std/ptr/ based on https://github.com/rust-lang/rust/pull/103996
-/// "The result of casting a reference to a pointer is valid for as long as the underlying object is live and no
-/// reference (just raw pointers) is used to access the same memory. That is, reference and pointer accesses
-/// cannot be interleaved."
-///
-/// In this regard, I consider the combination of Scope, RefScope, and Deref means there will certainly be interleaved use
-/// of references and pointers to the same underlying object. But if RefScope is a glorified &'scope T,
-/// then I do not understand the problem.
-///
-/// see also https://github.com/rust-lang/unsafe-code-guidelines/issues/463
-///
-/// Note: I am not so sure what is meant by "access". Since the RefScope pointer is never used to read/write, and is only used
-/// to construct new references by Deref, then maybe there is no issue either way.
+/// I currently strongly believe that this should be completely sound. RefScope is a transparent wrapper around T,
+/// but will only ever exist as a reference, making it useful as &'scope T.
 #[repr(transparent)]
-pub struct RefScope<S, T> {
-    _handle: *const T,
-    _scope: PhantomData<S>,
+pub struct RefScope<S, T: ?Sized> {
+    scope: PhantomData<S>,
+    handle: T,
 }
 
-impl<S, H> RefScope<S, H> {
-    /// Get the original Scope<'_, H>
+impl<S, T> RefScope<S, T> {
+    /// Get the original Scope<'_, T>
     pub(crate) fn as_scope(&self) -> S {
-        // this is sound because
-        // - we only construct RefScope as RefScope<Scope<'scope, H>, H>
-        // - Scope and RefScope are compatible types as discussed in Deref for Scope
-        // - thus, this is just reverting back to the original Scope
-        unsafe { std::mem::transmute_copy(self) }
+        // here, we want to reverse the Deref of Scope<'_, T>
+        // such as by transmute::<&RefScope<S, T>, S> (see Deref for Scope)
+        // but the compiler cannot know that S is correctly sized for all S
+        // Thus, we need to even more unsafely use transmute_copy
+        // This is sound so long as RefScope only exists as a result of Deref of Scope
+        unsafe { std::mem::transmute_copy(&self) }
     }
 
     /// manually get Deref target
     ///
     /// this is helpful because rust-analyzer seems to have trouble with autocompletion
     /// in some more complex uses of RefScope, when going through auto-deref
-    pub(crate) fn deref(&self) -> &H {
-        &**self
+    pub(crate) fn deref(&self) -> &T {
+        &self
     }
 }
 
 impl<S, H> std::ops::Deref for RefScope<S, H> {
     type Target = H;
     fn deref(&self) -> &Self::Target {
-        // this is safe because:
-        // - we only construct RefScope from Deref of Scope
-        // - the S type parameter captures the lifetime information of the Scope
-        // - thus, life of Scope >= RefScope<Scope, H>
-        // - life of H >= Scope >= RefScope
-        // - thus, we can create a &H with the shorter life of RefScope (from elided lifetime)
-        unsafe { &*self._handle }
+        &self.handle
     }
 }
-
-impl<S, T> Clone for RefScope<S, T> {
-    fn clone(&self) -> Self {
-        RefScope {
-            _handle: self._handle,
-            _scope: PhantomData,
-        }
-    }
-}
-
-impl<S, T> Copy for RefScope<S, T> {}
 
 /// Represents an async fn which produces a Future that is restricted to 'scope
 /// but can produce an output O that is not limited to 'scope
