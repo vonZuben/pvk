9215bc4bbf327f504978b29c51697af44b9c3d1a
Chris von Zuben <chris.vonzuben@gmail.com>
2024-06-25 17:28:43 +0900

    Make PipelineCacheUUID type

--- vk-safe/src/dispatchable_handles/physical_device/get_physical_device_properties.rs
+++ vk-safe/src/dispatchable_handles/physical_device/get_physical_device_properties.rs
@@ -71,9 +71,37 @@ const _VUID: () = {
 
 simple_struct_wrapper_scoped!(PhysicalDeviceProperties impl Deref);
 
+#[repr(transparent)]
+/// Universally unique identifier for a PhysicalDevice
+pub struct PipelineCacheUUID {
+    uuid: [u8; vk::UUID_SIZE],
+}
+
+impl std::fmt::Debug for PipelineCacheUUID {
+    /// print the UUID
+    ///
+    /// outputs the UUID with each byte printed in upper hex and 0 padded
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CacheUUID[")?;
+        for b in self.uuid {
+            write!(f, "{:0<2X}", b)?;
+        }
+        write!(f, "]")
+    }
+}
+
 impl<S> PhysicalDeviceProperties<S> {
     pretty_version!(api_version);
     get_str!(device_name);
+
+    /// Get the `PipelineCacheUUID` for this PhysicalDevice
+    pub fn pipeline_cache_uuid(&self) -> &PipelineCacheUUID {
+        unsafe {
+            std::mem::transmute::<&[u8; vk::UUID_SIZE], &PipelineCacheUUID>(
+                &self.pipeline_cache_uuid,
+            )
+        }
+    }
 }
 
 impl<S> fmt::Debug for PhysicalDeviceProperties<S> {
@@ -85,7 +113,7 @@ impl<S> fmt::Debug for PhysicalDeviceProperties<S> {
             .field("device_id", &self.device_id)
             .field("device_type", &self.device_type)
             .field("device_name", &self.device_name())
-            .field("pipeline_cache_id", &self.pipeline_cache_uuid)
+            .field("pipeline_cache_id", &self.pipeline_cache_uuid())
             .field("limits", &self.limits)
             .field("sparse_properties", &self.sparse_properties)
             .finish()
