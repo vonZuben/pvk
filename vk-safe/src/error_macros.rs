4a1b0ed994da4d2b8f0380722867d951b7b93fac
Chris von Zuben <chris.vonzuben@gmail.com>
2024-07-03 00:12:07 +0900

    extend error macros

--- vk-safe/src/error_macros.rs
+++ vk-safe/src/error_macros.rs
@@ -1,7 +1,35 @@
 macro_rules! unit_error {
-    ( $vis:vis $name:ident ) => {
+    (
+        $(#[$($attributes:tt)*])*
+        $vis:vis $name:ident
+    ) => {
         #[derive(Debug)]
+        $(#[$($attributes)*])*
         $vis struct $name;
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> std::fmt::Result {
+                std::fmt::Debug::fmt(self, f)
+            }
+        }
+
+        impl std::error::Error for $name {}
+    };
+}
+
+macro_rules! enum_error {
+    (
+        $(#[$($attributes:tt)*])*
+        $vis:vis enum $name:ident {
+            $( $variant:ident ),+
+            $(,)?
+        }
+    ) => {
+        #[derive(Debug)]
+        $(#[$($attributes)*])*
+        $vis enum $name {
+            $( $variant ),+
+        }
 
         impl std::fmt::Display for $name {
             fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> std::fmt::Result {
