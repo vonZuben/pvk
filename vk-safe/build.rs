cfd58673add0ba83c39a413e3db019ae19d4a544
Chris von Zuben <chris.vonzuben@gmail.com>
2024-08-19 15:36:34 +0900

    add check-cfg to build.rs

--- vk-safe/build.rs
+++ vk-safe/build.rs
@@ -3,5 +3,6 @@ use vk_safe_sys::features_and_extensions;
 fn main() {
     for fe in features_and_extensions() {
         println!("cargo::rustc-cfg={}", fe);
+        println!("cargo::rustc-check-cfg=cfg({})", fe);
     }
 }
